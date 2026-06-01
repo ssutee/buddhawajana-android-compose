434fb1f8b3d8a658ca2485468c3d407fb9a794e0
Sutee Sudprasert <sutee.s@gmail.com>
2026-06-01 20:25:39 +0700

    refactor(core:network): split Moshi/OkHttp into separate Koin singles (review fix)

--- core/network/src/main/java/com/watnapp/buddhawajana/core/network/NetworkModule.kt
+++ core/network/src/main/java/com/watnapp/buddhawajana/core/network/NetworkModule.kt
@@ -9,28 +9,25 @@ import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
 val networkModule = module {
+    single { Moshi.Builder().add(KotlinJsonAdapterFactory()).build() }
     single {
-        val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
-        val client = OkHttpClient.Builder()
+        OkHttpClient.Builder()
             .addInterceptor(HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BASIC })
             .build()
-        moshi to client
     }
     single<BookService> {
-        val (moshi, client) = get<Pair<Moshi, OkHttpClient>>()
         Retrofit.Builder()
             .baseUrl("http://etipitaka.org/ebookshop/oauth/")
-            .client(client)
-            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .client(get<OkHttpClient>())
+            .addConverterFactory(MoshiConverterFactory.create(get<Moshi>()))
             .build()
             .create(BookService::class.java)
     }
     single<AudioService> {
-        val (moshi, client) = get<Pair<Moshi, OkHttpClient>>()
         Retrofit.Builder()
             .baseUrl("http://watnapahpong.com/api/")
-            .client(client)
-            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .client(get<OkHttpClient>())
+            .addConverterFactory(MoshiConverterFactory.create(get<Moshi>()))
             .build()
             .create(AudioService::class.java)
     }
